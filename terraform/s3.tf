d409d5d30f035c28e2f29ff15d86734ad565632a
Uken49 <heldergavioes@hotmail.com>
2025-11-19 17:04:05 -0300

    terminando aplicação

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,12 +1,12 @@
 # S3 Bucket
 resource "aws_s3_bucket" "s3-original" {
-  bucket = "tcc-original-bucket"
+  bucket = "tcc-bucket-original"
 }
 
 resource "aws_s3_bucket" "s3-tratada" {
-  bucket = "tcc-tratada-bucket"
+  bucket = "tcc-bucket-tratada"
 }
 
 resource "aws_s3_bucket" "s3-reconhecida" {
-  bucket = "tcc-reconhecida-bucket"
+  bucket = "tcc-bucket-reconhecida"
 }
--- terraform/scripts/database-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Iniciando setup" > /var/log/setup.log
-sudo apt update -y
-sudo apt install -y postgresql
